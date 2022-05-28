e44bf7c212e1c7015ff86e55065476f87ce279fe
James <ponce800@hotmail.com>
2022-05-27 20:18:04 -0400

    testing portfolio website

---
+++ build/precache-manifest.171849dd7d863ca4a0679db92a0b69eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08b4bb9c973080b26933b6da019cf6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5196c391786edc93f9fb",
+    "url": "/static/css/2.d4cb1e1d.chunk.css"
+  },
+  {
+    "revision": "c54ba5c3c742a17e0ccb",
+    "url": "/static/css/main.c79df766.chunk.css"
+  },
+  {
+    "revision": "5196c391786edc93f9fb",
+    "url": "/static/js/2.9c0c86c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9c0c86c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54ba5c3c742a17e0ccb",
+    "url": "/static/js/main.eaa0439a.chunk.js"
+  },
+  {
+    "revision": "f18f79fc524fdfdc9d7c",
+    "url": "/static/js/runtime-main.9f1eb497.js"
+  }
+]);
